e2413971696b276368aea4874b5518cebaf8ced3
Waiariki Koia <waiariki.koia@rvu.co.uk>
2020-01-20 14:40:50 +0000

    Update README.md with a how to use section and update variables

--- variables.tf
+++ variables.tf
@@ -1,16 +1,21 @@
 variable "aws_vpc_id" {
-  description = "VPC to connect to/from"
+  description = "AWS VPC to connect to/from"
   type        = string
 }
 variable aws_region {
   description = "Default to Ireland region."
   default     = "eu-west-1"
 }
-variable bgp_asn {}
+variable bgp_asn {
+  default = "65000"
+  description = "Any private ASN (64512-65534, 4200000000-4294967294) that you are not already using in the peer network. The ASN is used for all BGP sessions on the same Cloud Router, and it cannot be changed later."
+}
 
-variable gcp_network_name {}
-variable gcp_subnet1_cidr {}
-variable gcp_subnet2_cidr {}
+variable gcp_network_name {
+  description = "GCP Network to connect to/from"
+}
+
 variable gcp_region {
-  default = "europe-west1"
+  description = "Default to London region."
+  default     = "europe-west2"
 }
